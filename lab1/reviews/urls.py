a630f8451aa4d251243336115bd779d8bd4daa0c
aladelca <alarcon.adrianc@gmail.com>
2025-01-29 20:47:16 -0500

    add and list reviews

--- lab1/reviews/urls.py
+++ lab1/reviews/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('list/', views.list_review, name = 'list_review'),
+    path('add/', views.add_review, name = 'add_review'),
 ]
--- lab1/reviews/views.py
+++ lab1/reviews/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
 
 def index(request):
     params = {}
     return render(request, 'index.html', params)
+
+def list_review(request):
+    reviews = Review.objects.all()
+    params = {'reviews': reviews}
+    return render(request, 'list_review.html', params)
+
+def add_review(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_review')
+    else:
+        form = ReviewForm()
+    params = {'form': form}
+    return render(request, 'add_review.html', params)
